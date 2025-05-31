b94f726878971801f3fad7c9e574f68bdcca2c19
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-05-31 13:51:54 +0300

    Update index.tsx

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -33,7 +33,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
 
         {/* ✅ posts.map for dynamic rendering */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {users.map((user) => (
+          {posts.map((user) => (
             <UserCard key={user.id} user={user} />
           ))}
         </div>
